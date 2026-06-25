0e298c4627bd4ff14c7cf728799219862939f678
Juuan159 <0001228451@senaimgaluno.com.br>
2026-06-25 16:33:58 -0300

    Departamentos

---
+++ Fun/Departamento.cs
@@ -0,0 +1,10 @@
+namespace ATTGit.Fun;
+
+        public class Departamento   
+{
+    public int Id { get; set; }  
+    public string Nome { get; set; }
+    public string Sigla { get; set; }
+    public int Ramal { get; set; }    
+    public int IdGerente { get; set; } 
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,55 @@
-﻿class Program
+﻿using System;
+using System.Linq;
+
+namespace ATTGit.Fun;
+    class Program
 {
-    static void  Main()
+    static void Main()
     {
+        List<Departamento> departamentos = new List<Departamento>();
+        int id = 0;
+
+        Console.WriteLine("Registre os Departamentos e seus dados respectivamente:");
         
+        while (true)
+        {
+            id++;
+            Departamento Contador = new Departamento();
+
+            Console.WriteLine($"Id do Departamento é {id}!");
+
+            Contador.Id = id;
+
+            Console.WriteLine("Nome do Departamento:");
+            string nome = Console.ReadLine();
+
+            Contador.Nome = nome;
+
+            Console.WriteLine("Sigla do Departamento:");
+            string sigla = Console.ReadLine();
+
+            Contador.Sigla = sigla;
+
+            Console.WriteLine("Id do Gerente responsável pelo Departamento:");
+            int idgerente = int.Parse(Console.ReadLine());
+
+            Contador.IdGerente = idgerente;
+
+            Console.WriteLine("Entre com o Ramal do Departamento:");
+            int ramal = int.Parse(Console.ReadLine());
+
+            Contador.Ramal = ramal;
+
+            Console.WriteLine("Digite um Número qualquer se deseja adicionar mais um Departamento ou 0 se deseja encerrar:");
+            int esc = int.Parse(Console.ReadLine());
+            if (esc == 0)
+            {
+                id = 0;
+                break;
+            }
+
+        
+
+    
     }
 }
